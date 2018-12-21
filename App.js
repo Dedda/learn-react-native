cddb14ab9ec9e2a3729ee390848c28ec044d7182
Stephan Goeppentin <dedda102@gmail.com>
2018-12-21 14:20:23 +0100

    simple stopwatch counter

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+class Counter extends React.Component {
+
+  state = {count: 0}
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({count: this.state.count + 1})
+    }, 1000)
+  }
+
+  render() {
+    const {count} = this.state
+    const {color, size} = this.props
+
+    return (
+      <Text style={{color, fontSize: size}}>
+        {count}
+      </Text>
+    )
+  }
+}
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Counter color={'lightblue'} size={16} />
+        <Counter color={'skyblue'} size={32} />
+        <Counter color={'steelblue'} size={80} />
+        <Counter color={'darkblue'} size={140} />
       </View>
-    );
+    )
   }
 }
 
