b2fe04c28897a7040639f0ee079053f922b92dee
Stephan Goeppentin <dedda102@gmail.com>
2018-12-21 14:50:18 +0100

    flex playground app

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import Toggle from './Toggle'
 
 export default class App extends React.Component {
+
+  state = {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
   render() {
+    const {flexDirection, alignItems, justifyContent} = this.state
+    const layoutStyle = {flexDirection, justifyContent, alignItems}
+
+    const primaryAxis = flexDirection === 'row' ? 'Horizontal': 'Vertical'
+    const secondaryAxis = flexDirection === 'row' ? 'Vertical': 'Horizontal'
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={{height: StatusBar.currentHeight}}/>
+        <Toggle label={'Primary axis (flexDirection)'}
+          value={flexDirection}
+          options={['row', 'column']}
+          onChange={(option) => this.setState({flexDirection: option})}/>
+        <Toggle label={primaryAxis + ' distribution (justifyContent)'}
+          value={justifyContent}
+          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
+          onChange={(option) => this.setState({justifyContent: option})}/>
+        <Toggle label={secondaryAxis + ' alignment (alignItems)'}
+          value={alignItems}
+          options={['flex-start', 'center', 'flex-end', 'stretch']}
+          onChange={(option) => this.setState({alignItems: option})}/>
+        <View style={[styles.layout, layoutStyle]}>
+          <View style={styles.box}/>
+          <View style={styles.box}/>
+          <View style={styles.box}/>
+        </View>
       </View>
-    );
+    )
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)'
+  },
+  box: {
+    padding: 25,
+    backgroundColor: 'steelblue',
+    margin: 5
+  }
 });
